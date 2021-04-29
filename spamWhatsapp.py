97124801a60b39db6ec4eb5e69a4c9c0dcfccce1
aayu17 <83199848+aayu17@users.noreply.github.com>
2021-04-29 12:18:57 +0530

    Add files via upload

---
+++ spamWhatsapp.py
@@ -0,0 +1,27 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("https://web.whatsapp.com/")
+time.sleep(5)
+
+for i in range(10):
+    pyautogui.press('F')
+    pyautogui.press('O')
+    pyautogui.press('L')
+    pyautogui.press('L')
+    pyautogui.press('O')
+    pyautogui.press('W')
+    pyautogui.press(' ')
+    pyautogui.press('@')
+    pyautogui.press('n')
+    pyautogui.press('e')
+    pyautogui.press('x')
+    pyautogui.press('t')
+    pyautogui.press('_')
+    pyautogui.press('c')
+    pyautogui.press('o')
+    pyautogui.press('d')
+    pyautogui.press('e')
+    pyautogui.press('r')
+    pyautogui.press('s')
+    pyautogui.press('enter')
